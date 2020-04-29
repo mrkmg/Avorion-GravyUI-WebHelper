1ecfe5258f6106751000fa3282b4f26e8db3582a
Kevin Gravier <kevin@mrkmg.com>
2020-04-29 17:48:03 -0400

    Tweaks

--- lua/examples/dialog.lua
+++ lua/examples/dialog.lua
@@ -3,7 +3,7 @@ local Node = include("gravyui/node")
 local x, y = getResolution()
 local width, height = 300, 300
 
-local window = Node(width, height):offset(x/2-width/2, x/2-height/2)
+local window = Node(width, height):offset(x/2-width/2, y/2-height/2)
 local title, body, buttons = window:pad(5):rows({40, 1, 40}, 5)
 buttons = {buttons:pad(2/5, 0, 0, 0):cols(2, 10)}
 
